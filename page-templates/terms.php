7b83c9081058a6017a8c582478dd0b4937162369
Chris <cmaust@nu.edu>
2020-05-06 11:54:48 -0700

    Adds terms page template

---
+++ page-templates/terms.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: Terms
+ *
+ * @package nusa
+ */
+
+get_header();
+?>
+	<main id="main" class="site-main">
+		<section class="section section--fluid">
+			<div class="container--fluid">
+				<div class="row">
+					<?php get_template_part( 'template-parts/hero' ); ?>
+					<?php get_template_part( 'template-parts/form' ); ?>
+
+					<?php if ( ! empty( get_the_content( null, false, get_the_ID() ) ) ) { ?>
+						<section class="section section--content">
+							<div class="container">
+								<div class="row">
+									<?php
+									while ( have_posts() ) :
+										the_post();
+										get_template_part( 'template-parts/content/content', 'page' );
+									endwhile; // End of the loop.
+									?>
+								</div>
+							</div>
+						</section>
+					<?php } ?>
+
+					<?php get_template_part( 'template-parts/widgets' ); ?>
+
+					<?php get_template_part( 'template-parts/quote', 'terms' ); ?>
+
+					<?php get_template_part( 'template-parts/accolades' ); ?>
+				</div>
+			</div>
+		</section>
+	</main><!-- #main -->
+	</main><!-- #main -->
+<?php
+get_footer();
